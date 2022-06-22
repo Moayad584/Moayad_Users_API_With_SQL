a64fab9818f11aa132570d6c3f571ea5227d3ad8
Moayad <moayad.hakim@gmail.com>
2022-06-22 10:10:46 +0300

    Add project files.

---
+++ Controllers/UserInController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoayadAPI.Data;
+using MoayadAPI.Models;
+
+namespace MoayadAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserInController : ControllerBase
+    {
+        private readonly Cloth_Factory_MoayadContext _context;
+
+        public UserInController(Cloth_Factory_MoayadContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserIn
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserInfo>>> GetUserInfos()
+        {
+          if (_context.UserInfos == null)
+          {
+              return NotFound();
+          }
+            return await _context.UserInfos.ToListAsync();
+        }
+
+        // GET: api/UserIn/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserInfo>> GetUserInfo(int id)
+        {
+          if (_context.UserInfos == null)
+          {
+              return NotFound();
+          }
+            var userInfo = await _context.UserInfos.FindAsync(id);
+
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return userInfo;
+        }
+
+        // PUT: api/UserIn/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserInfo(int id, UserInfo userInfo)
+        {
+            if (id != userInfo.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userInfo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserInfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserIn
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserInfo>> PostUserInfo(UserInfo userInfo)
+        {
+          if (_context.UserInfos == null)
+          {
+              return Problem("Entity set 'Cloth_Factory_MoayadContext.UserInfos'  is null.");
+          }
+            _context.UserInfos.Add(userInfo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserInfo", new { id = userInfo.UserId }, userInfo);
+        }
+
+        // DELETE: api/UserIn/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserInfo(int id)
+        {
+            if (_context.UserInfos == null)
+            {
+                return NotFound();
+            }
+            var userInfo = await _context.UserInfos.FindAsync(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserInfos.Remove(userInfo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserInfoExists(int id)
+        {
+            return (_context.UserInfos?.Any(e => e.UserId == id)).GetValueOrDefault();
+        }
+    }
+}
